3c283f42a19abaaf4afdd75533eae104625bf85b
VarshaSameer <km.varsha@gmail.com>
2020-12-29 15:26:42 +0530

    Updated to display Firebase Login Screen based on auth state

--- app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
+++ app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
@@ -1,8 +1,16 @@
 package com.udacity.project4.authentication
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
 import com.udacity.project4.R
+import com.udacity.project4.databinding.ActivityAuthenticationBinding
+import com.udacity.project4.locationreminders.RemindersActivity
 
 /**
  * This class should be the starting point of the app, It asks the users to sign in / register, and redirects the
@@ -10,20 +18,53 @@ import com.udacity.project4.R
  */
 class AuthenticationActivity : AppCompatActivity() {
 
+    companion object{
+        const val SIGN_IN_CODE = 1001
+    }
 
+    private lateinit var binding : ActivityAuthenticationBinding
+
+    private val auth = FirebaseAuth.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_authentication)
+        if(auth.currentUser == null) {
+            binding = DataBindingUtil.setContentView(this, R.layout.activity_authentication)
+            binding.btnLogin.setOnClickListener {
+                launchSigninFlow()
+            }
+        }
+        else{
+            startActivity(Intent(this, RemindersActivity::class.java))
 
-
-//          TODO: If the user was authenticated, send him to RemindersActivity
-
+        }
+        
 //          TODO: a bonus is to customize the sign in flow to look nice using :
         //https://github.com/firebase/FirebaseUI-Android/blob/master/auth/README.md#custom-layout
 
     }
 
 
+    private fun launchSigninFlow() {
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.EmailBuilder().build(), AuthUI.IdpConfig.GoogleBuilder().build()
+        )
+
+        startActivityForResult(
+            AuthUI.getInstance().createSignInIntentBuilder().setAvailableProviders(providers).build(), SIGN_IN_CODE
+        )
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode == SIGN_IN_CODE){
+            val response = IdpResponse.fromResultIntent(data)
+
+            if(resultCode == Activity.RESULT_OK){
+                startActivity(Intent(this, RemindersActivity::class.java))
+            }
+        }
+    }
 
 }
