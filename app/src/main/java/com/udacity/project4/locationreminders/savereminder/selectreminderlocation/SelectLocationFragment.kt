92682dc626e5ead644dfde6040453cb786834628
VarshaSameer <km.varsha@gmail.com>
2021-01-13 16:21:37 +0530

    Added the following: the map setup implementation, zoom to the user location after taking permission, put a marker to location that the user selected, change the map type based on the user's selection

--- app/src/main/java/com/udacity/project4/locationreminders/savereminder/selectreminderlocation/SelectLocationFragment.kt
+++ app/src/main/java/com/udacity/project4/locationreminders/savereminder/selectreminderlocation/SelectLocationFragment.kt
@@ -3,17 +3,31 @@ package com.udacity.project4.locationreminders.savereminder.selectreminderlocati
 
 import android.Manifest
 import android.content.IntentSender
+import android.content.pm.PackageManager
+import android.location.Geocoder
+import android.location.Location
 import android.os.Build
 import android.os.Bundle
 import android.view.*
+import android.widget.Toast
 import androidx.core.content.PermissionChecker
 import androidx.databinding.DataBindingUtil
 import com.google.android.gms.common.api.ResolvableApiException
+import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationRequest
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.location.LocationSettingsRequest
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
 import com.udacity.project4.R
 import com.udacity.project4.base.BaseFragment
+import com.udacity.project4.base.NavigationCommand
 import com.udacity.project4.databinding.FragmentSelectLocationBinding
 import com.udacity.project4.locationreminders.savereminder.SaveReminderViewModel
 import com.udacity.project4.utils.setDisplayHomeAsUpEnabled
@@ -21,11 +35,25 @@ import org.koin.android.ext.android.inject
 import timber.log.Timber
 
 
-class SelectLocationFragment : BaseFragment() {
+class SelectLocationFragment : BaseFragment(), OnMapReadyCallback {
 
     //Use Koin to get the view model of the SaveReminder
     override val _viewModel: SaveReminderViewModel by inject()
     private lateinit var binding: FragmentSelectLocationBinding
+
+    private lateinit var map: GoogleMap
+    private var lastKnownLocation: Location? = null
+    private var selectedMarker: Marker? = null
+    private var cameraPosition: CameraPosition? = null
+
+
+    // The entry point to the Fused Location Provider.
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+
+    // A default location (Sydney, Australia) and default zoom to use when location permission is
+    // not granted.
+    private val defaultLocation = LatLng(-33.8523341, 151.2106085)
+    private var locationPermissionGranted = false
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
@@ -39,23 +67,25 @@ class SelectLocationFragment : BaseFragment() {
         setHasOptionsMenu(true)
         setDisplayHomeAsUpEnabled(true)
 
-//        TODO: add the map setup implementation
-//        TODO: zoom to the user location after taking his permission
 //        TODO: add style to the map
-//        TODO: put a marker to location that the user selected
-        checkAndRequestLocationPermissions()
 
+        // Construct a FusedLocationProviderClient.
+        fusedLocationProviderClient =
+            LocationServices.getFusedLocationProviderClient(requireActivity())
 
-//        TODO: call this function after the user confirms on the selected location
-        onLocationSelected()
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
+        mapFragment?.getMapAsync(this)
 
         return binding.root
     }
 
-    private fun onLocationSelected() {
-        //        TODO: When the user confirms on the selected location,
-        //         send back the selected location details to the view model
-        //         and navigate back to the previous fragment to save the reminder and add the geofence
+    private fun onLocationSelected(latLng: LatLng) {
+        //        TODO: add the geofence
+        val fromLocation = Geocoder(activity).getFromLocation(latLng.latitude, latLng.longitude, 2)
+        _viewModel.reminderSelectedLocationStr.postValue(fromLocation[0].locality)
+        _viewModel.latitude.postValue(latLng.latitude)
+        _viewModel.longitude.postValue(latLng.longitude)
+        _viewModel.navigationCommand.postValue(NavigationCommand.Back)
     }
 
 
@@ -64,17 +94,20 @@ class SelectLocationFragment : BaseFragment() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
-        // TODO: Change the map type based on the user's selection.
         R.id.normal_map -> {
+            map.mapType = GoogleMap.MAP_TYPE_NORMAL
             true
         }
         R.id.hybrid_map -> {
+            map.mapType = GoogleMap.MAP_TYPE_HYBRID
             true
         }
         R.id.satellite_map -> {
+            map.mapType = GoogleMap.MAP_TYPE_SATELLITE
             true
         }
         R.id.terrain_map -> {
+            map.mapType = GoogleMap.MAP_TYPE_TERRAIN
             true
         }
         else -> super.onOptionsItemSelected(item)
@@ -87,15 +120,63 @@ class SelectLocationFragment : BaseFragment() {
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
 
-        checkDeviceLocationSettings(true)
+        locationPermissionGranted = false
+        when (requestCode) {
+            REQUEST_CODE_LOCATION -> {
 
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.isNotEmpty() &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    locationPermissionGranted = true
+                    Timber.d("Permission Granted")
+                    checkDeviceLocationSettings(true)
+                }
+            }
+        }
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        map = googleMap
+
+        updateLocationUI()
+        map?.setOnMapClickListener { latLng ->
+            // Get the current location of the device and set the position of the map.
+            selectedMarker?.remove()
+            selectedMarker?.position = latLng
+            map?.addMarker(MarkerOptions().position(latLng)).also {
+                selectedMarker = it
+            }
+            map?.animateCamera(CameraUpdateFactory.newLatLng(latLng))
+            onLocationSelected(latLng)
+        }
+    }
+
+    private fun updateLocationUI() {
+        if (map == null) {
+            return
+        }
+        try {
+            if (locationPermissionGranted) {
+                map?.isMyLocationEnabled = true
+                map?.uiSettings?.isMyLocationButtonEnabled = true
+            } else {
+                map?.isMyLocationEnabled = false
+                map?.uiSettings?.isMyLocationButtonEnabled = false
+                lastKnownLocation = null
+                checkAndRequestLocationPermissions()
+            }
+        } catch (e: SecurityException) {
+            Timber.d("Exception: %s", e.message)
+        }
     }
 
     private fun checkAndRequestLocationPermissions() {
         if (foregroundAndBackgroundLocationPermissionApproved()) {
             // Have permissions, do things!
-//        Toast.makeText(context, "TODO: Location", Toast.LENGTH_LONG).show()
             checkDeviceLocationSettings()
+            locationPermissionGranted = true
+            updateLocationUI()
+            getDeviceLocation()
 
         } else {
             requestPermissions(LOCATION_PERMISSIONS, REQUEST_CODE_LOCATION)
@@ -129,6 +210,9 @@ class SelectLocationFragment : BaseFragment() {
         val settingsClient = LocationServices.getSettingsClient(requireActivity())
         val locationSettingsResponseTask =
             settingsClient.checkLocationSettings(builder.build())
+        locationSettingsResponseTask.addOnSuccessListener {
+            getDeviceLocation()
+        }
         locationSettingsResponseTask.addOnFailureListener { exception ->
             Timber.d("addOnFailure")
 
@@ -149,11 +233,54 @@ class SelectLocationFragment : BaseFragment() {
         }
     }
 
-    companion object {
-        const val REQUEST_CODE_LOCATION = 125
-        const val REQUEST_TURN_DEVICE_LOCATION_ON = 126
+    private fun getDeviceLocation() {
+        /*
+         * Get the best and most recent location of the device, which may be null in rare
+         * cases when a location is not available.
+         */
+        try {
+            if (locationPermissionGranted) {
+                val locationResult = fusedLocationProviderClient.lastLocation
+                locationResult.addOnCompleteListener(requireActivity()) { task ->
 
-        val LOCATION_PERMISSIONS = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                    if (task.isSuccessful) {
+
+                        // Set the map's camera position to the current location of the device.
+                        lastKnownLocation = task.result
+                        if (lastKnownLocation != null) {
+                            Timber.d("last  known location $lastKnownLocation")
+
+                            map?.moveCamera(
+                                CameraUpdateFactory.newLatLngZoom(
+                                    LatLng(
+                                        lastKnownLocation!!.latitude,
+                                        lastKnownLocation!!.longitude
+                                    ), DEFAULT_ZOOM.toFloat()
+                                )
+                            )
+                        } else {
+                            Timber.e("Exception: %s", task.exception)
+                            map?.moveCamera(
+                                CameraUpdateFactory
+                                    .newLatLngZoom(defaultLocation, DEFAULT_ZOOM.toFloat())
+                            )
+                            map?.uiSettings?.isMyLocationButtonEnabled = false
+                        }
+                    }
+                }
+            }
+        } catch (e: SecurityException) {
+            Timber.e("Exception: %s", e.message)
+        }
+    }
+
+    companion object {
+        private const val DEFAULT_ZOOM = 16
+
+        private const val REQUEST_CODE_LOCATION = 125
+        private const val REQUEST_TURN_DEVICE_LOCATION_ON = 126
+
+        private val LOCATION_PERMISSIONS = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
             arrayOf(
                 Manifest.permission.ACCESS_FINE_LOCATION,
                 Manifest.permission.ACCESS_BACKGROUND_LOCATION
