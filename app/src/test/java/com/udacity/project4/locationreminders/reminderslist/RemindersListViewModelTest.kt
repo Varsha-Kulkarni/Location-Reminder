35c6bc1b5aba1adb5a3051d68ae352c5400f9562
VarshaSameer <km.varsha@gmail.com>
2021-01-17 19:29:13 +0530

    Provided testing to the RemindersListViewModel and its live data objects

--- app/src/test/java/com/udacity/project4/locationreminders/reminderslist/RemindersListViewModelTest.kt
+++ app/src/test/java/com/udacity/project4/locationreminders/reminderslist/RemindersListViewModelTest.kt
@@ -1,13 +1,51 @@
 package com.udacity.project4.locationreminders.reminderslist
 
+import android.os.Build
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.google.common.truth.Truth.assertThat
+import com.udacity.project4.locationreminders.data.FakeDataSource
+import com.udacity.project4.locationreminders.data.dto.ReminderDTO
+import com.udacity.project4.locationreminders.util.getOrAwaitValue
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
+import org.robolectric.annotation.Config
+
+//to get rid of this error: java.lang.UnsupportedOperationException: Failed to create a Robolectric sandbox: Android SDK 30 requires Java 9 (have Java 8)
+//source: stackOverflow
+@Config(sdk = [Build.VERSION_CODES.O_MR1])
 
 @RunWith(AndroidJUnit4::class)
 @ExperimentalCoroutinesApi
 class RemindersListViewModelTest {
 
-    //TODO: provide testing to the RemindersListViewModel and its live data objects
+    private lateinit var remindersListViewModel : RemindersListViewModel
+    private lateinit var dataSource : FakeDataSource
 
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
+
+    @Before
+    fun setupViewModel(){
+
+        dataSource = FakeDataSource()
+
+        remindersListViewModel = RemindersListViewModel(ApplicationProvider.getApplicationContext(), dataSource)
+    }
+
+    @Test
+    fun getReminders_resultNotEmpty() = runBlockingTest{
+
+        val reminder = ReminderDTO("title","description","location",-33.8523341, 151.2106085)
+        dataSource.saveReminder(reminder)
+
+        remindersListViewModel.loadReminders()
+
+        assertThat(remindersListViewModel.remindersList.getOrAwaitValue()).isNotEmpty()
+    }
 }
